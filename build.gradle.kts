d48a589c04ef3c674bc69e3202a91fefea27ebbd
mudkipdev <mudkip@mudkip.dev>
2024-07-08 22:44:42 -0600

    make jar runnable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    application
 }
+
+application.mainClass = "com.mooncell07.cecc.MainKt"
+group = "com.mooncell07"
+version = "0.1.0"
 
 kotlin {
     jvmToolchain(21)
 }
-
-group = "com.mooncell07"
-version = "0.1.0"
 
 repositories {
     mavenCentral()
