e497c2b0d65df43eb8fe6203a5e73ddf13dc6745
Nova <80042274+mooncell07@users.noreply.github.com>
2024-07-12 11:48:53 +0530

    feat: add rest of the addressing modes

--- src/main/kotlin/com/mooncell07/cecc/core/CPU.kt
+++ src/main/kotlin/com/mooncell07/cecc/core/CPU.kt
@@ -22,20 +22,46 @@ open class Stream(private val reg: Register, private val bus: Bus) {
         return when (mode) {
             AM.IMMEDIATE -> fetch()
             AM.ACCUMULATOR -> reg[R.A]
-            AM.INDIRECT -> bus.readByte(fetchWord())
+            AM.ABSOLUTE -> bus.readByte(fetchWord())
+            AM.ABSOLUTE_X -> bus.readByte((fetchWord() + reg[R.X]).toUShort())
+            AM.ABSOLUTE_Y -> bus.readByte((fetchWord() + reg[R.Y]).toUShort())
 
             AM.X_INDIRECT -> {
+                val base = (fetch() + reg[R.X]).toUByte()
+                val ptrAddr = concat((base + 1u).toUByte(), base)
+                val ptr = bus.readWord(ptrAddr)
+                bus.readByte(ptr)
+            }
+
+            AM.INDIRECT_Y -> {
+                val base = fetch()
+                val ptrAddr = concat((base + 1u).toUByte(), base) + reg[R.Y]
+                val ptr = bus.readWord(ptrAddr.toUShort())
+                bus.readByte(ptr)
+            }
+
+            AM.ZEROPAGE -> bus.readByte(fetch().toUShort())
+
+            AM.ZEROPAGE_X -> {
                 val fullAddr = bus.readWord((fetch() + reg[R.X]).toUShort())
                 bus.readByte(fullAddr)
             }
 
-            AM.INDIRECT_Y -> {
-                val fullAddr = bus.readWord(fetch().toUShort()) + reg[R.Y]
-                bus.readByte(fullAddr.toUShort())
+            AM.ZEROPAGE_Y -> {
+                val fullAddr = bus.readWord((fetch() + reg[R.Y]).toUShort())
+                bus.readByte(fullAddr)
             }
 
             else -> 0xFFu
         }
+    }
+
+    fun readIndirect(): UShort {
+        return bus.readWord(fetchWord())
+    }
+
+    fun readRelative(): Byte {
+        return fetch().toByte()
     }
 }
 
