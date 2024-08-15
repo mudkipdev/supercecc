a80ae2679b59bef272cc6060fbcda20aed54fb9a
Nova <80042274+mooncell07@users.noreply.github.com>
2024-08-15 17:35:43 +0530

    feat: convert Cartridge to Device

--- src/main/kotlin/com/mooncell07/cecc/core/Cartridge.kt
+++ src/main/kotlin/com/mooncell07/cecc/core/Cartridge.kt
@@ -4,12 +4,15 @@ import java.io.File
 
 class Cartridge(
     filepath: String,
-) {
+) : AbstractDevice() {
+    override val type = DT.CARTRIDGE
+    override val size = 0x8000
+    override val base = 0x8000
     private val data: UByteArray = File(filepath).readBytes().toUByteArray()
 
-    fun readRom(address: UShort): UByte = data[address.toInt()]
+    override fun read(address: UShort): UByte = data[address.toInt()]
 
-    fun writeRom(
+    override fun write(
         address: UShort,
         data: UByte,
     ) {
